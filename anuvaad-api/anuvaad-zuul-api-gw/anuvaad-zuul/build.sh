3f4f7d2954ca12326389d9324850ead4689280af
chinnareddym3 <153263284+chinnareddym3@users.noreply.github.com>
2025-06-04 15:41:50 +0530

    mvn changes

--- anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/build.sh
+++ anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64
-export MAVEN_HOME=/opt/maven/bin/mvn
+export MAVEN_HOME=/opt/apache-maven-3.2.5
 mvn -version
 java --version
 mvn compile
@@ -10,6 +10,6 @@ mvn clean install
 commit_id=${BUILD_ID}-$(git rev-parse --short HEAD)
 #commit_id=$(git rev-parse --short HEAD)
 echo $commit_id> commit_id.txt
-docker build -t anuvaadio/$image_name:$commit_id .
-docker login -u $dockerhub_user -p $dockerhub_pass
-docker push anuvaadio/$image_name:$commit_id
+sudo docker build -t anuvaadio/$image_name:$commit_id .
+sudo docker login -u $dockerhub_user -p $dockerhub_pass
+sudo docker push anuvaadio/$image_name:$commit_id
